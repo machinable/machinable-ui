1bcd8abbd33d89f35c68ed6cf62b6b4c91e7f90a
Nick <nick.sjostrom12@gmail.com>
2018-10-25 23:24:57 -0400

    -

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -29,7 +29,7 @@ class SideBar extends Component {
                                     <NavLink to={"/project/"+slug+"/api"} style={{display: "flex"}}>
                                         <FontAwesomeIcon className="center-self" style={{fontSize: "24px"}} icon={faProject} fixedWidth/>
                                         <div className="margin-left-more">
-                                            API
+                                            <div className="vertical-align margin-right"><span>API</span><span className="tag background-accent margin-left" style={{"padding": "1px 5px"}}>beta</span></div>
                                             <p className="navlink-description">Manage defined API resources</p>
                                         </div>
                                     </NavLink>
